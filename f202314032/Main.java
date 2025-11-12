489f125ee2d1815f5167ba50477f3a94e202325a
Bae Yujin <ujini7833@gmail.com>
2025-11-12 12:41:05 +0900

    rebase test

--- f202314032/Main.java
+++ f202314032/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("배유진");
+        System.out.println("배유진 [A tweak]");
         World.run();
         World.run2();
         Hello.run();
+        System.out.println("B: feature line");
     }
 }
-// main update
