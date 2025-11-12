9c8d8e4c406f9dbe2bc7dfa4c8137c83ba264ba2
Bae Yujin <ujini7833@gmail.com>
2025-11-12 12:27:21 +0900

    five

--- f202314032/World.java
+++ f202314032/World.java
@@ -2,4 +2,8 @@ public class World {
     public static void run() {
         System.out.println("202314032");
     }
+
+    public static void run2() {
+        System.out.println("hello world");
+    }
 }
